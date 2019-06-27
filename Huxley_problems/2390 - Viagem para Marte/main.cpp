4c978fa0ac71473a00a9a1c919d62efe37df1077
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-06-27 16:17:57 -0300

    ...

--- Huxley_problems/2390 - Viagem para Marte/main.cpp
+++ Huxley_problems/2390 - Viagem para Marte/main.cpp
@@ -1,49 +1,72 @@
 #include <iostream>
 
+#define MAX_RANGE 10000000000
+
 using namespace std;
 
-#define INTERVAL 1000
+double makeTravel(double fuel, short  weight, short taken_off[], short take_off[], unsigned short size) {
+    unsigned short i;
 
-short *count_sort(short *array, short size) {
-    auto new_array = new short[size];
-    auto count = new short[INTERVAL] {0};
-    unsigned  int i;
-
-    for(i = 0; i < size; i ++) {
-        count[array[i]]++;
-    }
+    fuel -= (fuel + weight) / taken_off[0];
 
     for(i = 1; i < size; i++) {
-        count[i] += count[i - 1];
+        fuel -= (fuel + weight) / take_off[i];
+        fuel -= (fuel + weight) / taken_off[i];
     }
 
-    for(i = size - 1; i >= 0; i--) {
-        new_array[count[array[i]]] = array[i];
-        count[array[i]]--;
-    }
+    fuel -= (fuel + weight) / take_off[0];
 
-    delete [] count;
-    return  new_array;
+    return fuel;
+}
+
+double biSearch(double start, double end, short taken_off[], short take_off[], unsigned short size, unsigned short weight) {
+    double mid, aux;
+    while(start != end) {
+        mid = (start + end) / 2.0;
+        aux = makeTravel(mid, weight, taken_off, take_off, size);
+//        cout << start << " | " << end << " | " << mid << " | " << aux << "\n";
+
+        if(aux >= -0.000000001 && aux <= 0.000000001) {
+            return mid;
+        }
+
+        if(aux < 0) {
+            start = mid + 0.000000001;
+        }
+        else {
+            end = mid - 0.000000001;
+        }
+    }
 }
 
 int main() {
-    unsigned int i;
-    short n, m;
-    cin >> n >> m;
+    unsigned  short i;
+    short n = -1, m = -1;
+    cin >> n;
+    cin >> m;
 
-    auto a_s = new short[n];
-    auto b_s = new short[n];
-    short *o_a_s, *o_b_s;
-
-    for(i = 0; i < n; i++) {
-        cin >> a_s[i];
-    }
-    o_a_s = count_sort(a_s, n);
+    short taken_off[n];
+    short take_off[n];
 
     for(i = 0; i < n; i++) {
-        cin >> b_s[i];
+        cin >> taken_off[i];
     }
-    o_b_s =  count_sort(b_s, n);
+
+    for(i = 0; i < n; i++) {
+        cin >> take_off[i];
+    }
+
+    double aux = makeTravel(MAX_RANGE, m, taken_off, take_off, n);
+    if(aux < 0) {
+        cout << -1;
+    }
+    else if(aux > 0) {
+        printf("%.2lf", biSearch(0, MAX_RANGE, taken_off, take_off, n, m));
+    }
+    else {
+        cout << MAX_RANGE;
+    }
+
 
     return 0;
 }
